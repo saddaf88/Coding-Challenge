52050ca8e776d8ac8bac242f987135f66971dce2
Saddaf Afrin Khan <saddaf88@gmail.com>
2020-12-02 00:34:10 +0100

    (Daily-Challange) - HelloWorld

---
+++ HackerRank/HelloWorld.py
@@ -0,0 +1,13 @@
+# Input Format
+#
+# You do not need to read any input in this challenge.
+#
+# Output Format
+#
+# Print Hello, World! to stdout.
+#
+# Sample Output 0
+#
+# Hello, World!
+
+print("Hello, World!")
